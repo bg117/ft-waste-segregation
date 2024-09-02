91716357a08973e44b81df9b1b87e0f5cc89f053
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-09-02 22:46:48 +0800

    provide solenoid pair API for NOT-gate operation

--- controller.py
+++ controller.py
@@ -7,26 +7,33 @@ class UltrasonicArray:
     def __init__(self, txt):
         self.front = txt.ultrasonic(1) # I1
         self.bio = txt.ultrasonic(2) # I2
-        self.np_ultrasonic = txt.ultrasonic(3) # I3
-        self.rec_ultrasonic = txt.ultrasonic(4) # I4
+        self.np = txt.ultrasonic(3) # I3
+        self.rec = txt.ultrasonic(4) # I4
 
 class EncoderArray:
     def __init__(self, txt):
-        self.preface_enc = txt.motor(1) # M1, preface motor
-        self.main_enc = txt.motor(2) # M2, main motor
+        self.preface = txt.motor(1) # M1, preface motor
+        self.main = txt.motor(2) # M2, main motor
 
-class SolenoidInputValveArray:
-    def __init__(self, txt):
-        self.bio_input_valve = txt.output(2) # O2
-        self.np_input_valve = txt.output(3) # O3
-        self.rec_input_valve = txt.output(4) # O4
+class Solenoid:
+    def __init__(self, master, slave, index):
+        self._input = master.input(index) # I1, I2, I3
+        self._output = slave.output(index)
+
+    def open(self):
+        self._input.setLevel(512) # pwm full power
+        self._output.setLevel(0)
+
+    def close(self):
+        self._input.setLevel(0) # pwm full power
+        self._output.setLevel(512)
 
 # slave controller
-class SolenoidOutputValveArray:
-    def __init__(self, txt):
-        self.bio_output_valve = txt.output(1) # O1
-        self.np_output_valve = txt.output(2) # O2
-        self.rec_output_valve = txt.output(3) # O3
+class SolenoidArray:
+    def __init__(self, master, slave):
+        self.bio = Solenoid(master, slave, 1)
+        self.nc = Solenoid(master, slave, 2)
+        self.rec = Solenoid(master, slave, 3)
 
 class TXTController:
     def __init__(self):
@@ -38,9 +45,8 @@ class TXTController:
         print("TXT (slave) firmware version: ", self.txt2.getFirmwareVersion())
 
         self.ultrasonic = UltrasonicArray(self.txt1)
-        self.solenoid_input_valves = SolenoidInputValveArray(self.txt1)
-        self.solenoid_output_valves = SolenoidOutputValveArray(self.txt2)
-        self.encoders = EncoderArray(self.txt1)
+        self.solenoid = SolenoidArray(self.txt1, self.txt2)
+        self.encoder = EncoderArray(self.txt1)
 
         self.weight_sensor = self.txt1.input(5) # I5
-        self.compressor = self.txt1.output(1) # O1
+        self.compressor = self.txt1.output(4) # O4
