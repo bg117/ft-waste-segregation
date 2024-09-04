4aac1c36666fe825781ce21951a9960f670c97dd
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-09-04 20:10:56 +0800

    add input checking to test_controls

--- program.py
+++ program.py
@@ -10,5 +10,13 @@ def test_controls(txt: controller.TXTController):
     txt.encoder.preface.setSpeed(512) # start preface motor
     txt.encoder.main.setSpeed(512) # start main motor
 
+    w = txt.weight_sensor.state() # read weight sensor
+    u1 = txt.ultrasonic.front.distance() # read front ultrasonic sensor
+    u2 = txt.ultrasonic.bio.distance() # read bio ultrasonic sensor
+    u3 = txt.ultrasonic.np.distance() # read np ultrasonic sensor
+    u4 = txt.ultrasonic.rec.distance() # read rec ultrasonic sensor
+
+    print(f'PB Trigger: {w}, Front: {u1} cm, Bio: {u2} cm, NP: {u3} cm, Rec: {u4} cm')
+
 def main(txt: controller.TXTController):
     pass
