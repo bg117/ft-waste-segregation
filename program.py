ad0889586260d83e1970855d849c1a4e06372daf
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-09-04 21:02:17 +0800

    change .bio etc to array syntax and extract move_waste method

--- program.py
+++ program.py
@@ -1,12 +1,8 @@
 import time
 import controller
-from controller import pwm
+from controller import BIO, NP, REC, pwm
 
 US_THRESHOLD = 15
-
-def detect_waste(txt: controller.TXTController):
-    # array of bio, np, and rec types detected in the frame (from right to left)
-    return []
 
 def loop(txt: controller.TXTController):
     # wait until the weight sensor is triggered
@@ -18,16 +14,7 @@ def loop(txt: controller.TXTController):
         return
     
     # run the preface and main motors
-    txt.encoder.preface.setSpeed(512)
-    txt.encoder.main.setSpeed(512)
-
-    # wait until less than US_THRESHOLD
-    while txt.ultrasonic.object_confirm.distance() > US_THRESHOLD:
-        pass
-
-    # stop the preface and main motors
-    txt.encoder.preface.setSpeed(0)
-    txt.encoder.main.setSpeed(0)
+    move_waste(txt)
 
     array = detect_waste(txt)
     while len(array) > 0:
@@ -50,6 +37,22 @@ def loop(txt: controller.TXTController):
         txt.compressor.setLevel(pwm(False))
         txt.solenoid[type].close()
 
+def move_waste(txt: controller.TXTController):
+    txt.encoder.preface.setSpeed(512)
+    txt.encoder.main.setSpeed(512)
+
+    # wait until less than US_THRESHOLD
+    while txt.ultrasonic.object_confirm.distance() > US_THRESHOLD:
+        pass
+
+    # stop the preface and main motors
+    txt.encoder.preface.setSpeed(0)
+    txt.encoder.main.setSpeed(0)
+
+def detect_waste(txt: controller.TXTController):
+    # array of bio, np, and rec types detected in the frame (from right to left)
+    return []
+
 # --- essentials ---
 
 def main(txt: controller.TXTController):
@@ -59,9 +62,9 @@ def main(txt: controller.TXTController):
         loop(txt)
 
 def test_controls(txt: controller.TXTController):
-    txt.solenoid.bio.open()
-    txt.solenoid.nc.open()
-    txt.solenoid.rec.open()
+    txt.solenoid[BIO].open()
+    txt.solenoid[NP].open()
+    txt.solenoid[REC].open()
 
     txt.compressor.setLevel(512) # turn on compressor
     
@@ -70,8 +73,8 @@ def test_controls(txt: controller.TXTController):
 
     w = txt.weight_sensor.state() # read weight sensor
     u1 = txt.ultrasonic.front.distance() # read front ultrasonic sensor
-    u2 = txt.ultrasonic.bio.distance() # read bio ultrasonic sensor
-    u3 = txt.ultrasonic.np.distance() # read np ultrasonic sensor
-    u4 = txt.ultrasonic.rec.distance() # read rec ultrasonic sensor
+    u2 = txt.ultrasonic[BIO].distance() # read bio ultrasonic sensor
+    u3 = txt.ultrasonic[NP].distance() # read np ultrasonic sensor
+    u4 = txt.ultrasonic[REC].distance() # read rec ultrasonic sensor
 
     print(f'PB Trigger: {w}, Front: {u1} cm, Bio: {u2} cm, NP: {u3} cm, Rec: {u4} cm')
