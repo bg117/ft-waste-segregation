60ff995957bd86594a3cd8778e4424ebbc6637b8
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-09 21:17:21 +0800

    script

--- upload.ps1
+++ upload.ps1
@@ -1,1 +1,25 @@
-scp -r main.py lib ft@192.168.8.2:workspaces/ft
+param (
+    [string[]]$src,
+    [string]$dest,
+    [switch]$usb
+)
+
+# Determine IP address based on the first argument
+if ($usb) {
+    $ip = "192.168.7.2"
+}
+else {
+    $ip = "192.168.8.2"
+}
+
+$delimSrcs = '"' + ($src -join '" "') + '"'
+
+# Upload each file using SCP
+$scpCommand = "scp -r $delimSrcs ft@${ip}:$dest"
+Write-Host "Executing: $scpCommand"
+Invoke-Expression $scpCommand
+
+# SSH command to create a new session
+$sshCommand = "ssh ft@$ip"
+Write-Host "Starting SSH session: $sshCommand"
+Invoke-Expression $sshCommand
